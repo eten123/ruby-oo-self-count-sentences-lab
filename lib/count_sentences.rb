0186ce9de6e5533369c5dfcc595c2ff9fad9ef0c
Eten <eten.uket@yale.edu>
2021-06-20 03:49:04 -0400

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,22 @@ require 'pry'
 
 class String
 
+
   def sentence?
-    
+    self.end_with?(".")  #the code will evaluate this expression
+                          #if the end with is a period it automatically evalutates to true
   end
 
   def question?
-
-  end
+    self.end_with?("?") # we can use self because we are calling
+  end                   #the method on the string object by using self
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    new_array.length
   end
 end
